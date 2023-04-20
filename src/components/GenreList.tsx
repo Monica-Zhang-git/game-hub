4ae4eaeac8a233456be64ae718eb361df957a21c
Monica Zhang <monica.zhang.nz@gmail.com>
2023-04-20 12:31:31 +1200

    Highlight the selectedGenre

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -14,9 +14,10 @@ import GenreListSkeleton from "./GenreListSkeleton";
 
 interface Props {
   onSelectGenre: (genre: Genre) => void;
+  selectedGenre: Genre | null;
 }
 
-const GenreList = ({ onSelectGenre }: Props) => {
+const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
   const { data, isLoading } = useGenre();
   const skeletons = [1, 2, 3, 4, 5, 6];
 
@@ -25,25 +26,30 @@ const GenreList = ({ onSelectGenre }: Props) => {
       {isLoading &&
         skeletons.map((skeleton) => <GenreListSkeleton key={skeleton} />)}
       <List>
-        {data.map((genre) => (
-          <ListItem key={genre.id} paddingY="5px">
-            <HStack>
-              <Image
-                src={getCropppedImageUrl(genre.image_background)}
-                boxSize="32px"
-                overflow="hidden"
-                borderRadius={8}
-              />
-              <Button
-                onClick={() => onSelectGenre(genre)}
-                variant="link"
-                fontSize="lg"
-              >
-                {genre.name}
-              </Button>
-            </HStack>
-          </ListItem>
-        ))}
+        {data.map((genre) => {
+          return (
+            <ListItem key={genre.id} paddingY="5px">
+              <HStack>
+                <Image
+                  src={getCropppedImageUrl(genre.image_background)}
+                  boxSize="32px"
+                  overflow="hidden"
+                  borderRadius={8}
+                />
+                <Button
+                  fontWeight={
+                    genre.id === selectedGenre?.id ? "bold" : "normal"
+                  }
+                  onClick={() => onSelectGenre(genre)}
+                  variant="link"
+                  fontSize="lg"
+                >
+                  {genre.name}
+                </Button>
+              </HStack>
+            </ListItem>
+          );
+        })}
       </List>
     </SimpleGrid>
   );
